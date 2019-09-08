3759407f2d344e36b1934d4a82932b6359753657
Jarek Figura <jarek.figura@icloud.com>
2019-09-08 23:59:05 +0200

    display students "user name", "name" and "surname" in a table

--- src/components/business/Students.js
+++ src/components/business/Students.js
@@ -2,29 +2,46 @@ import React, { Component } from 'react';
 import { withXML } from '../contexts/XML';
 import StudentSearch from './StudentSearch';
 import organizeStudents from "./organizeStudents";
+import './Students.css';
 
 export class Students extends Component {
+  handleClick = () => { };
+
   render() {
     const students = organizeStudents.apply(this);
-    // const students = this.props.students;
+
     return (
       <div>
         <h2 style={{ textAlign: 'left' }}>Students found: {students.length}</h2>
-        <h3 style={{ textAlign: 'left' }}>Search students by user name: <StudentSearch/></h3>
-        {/* <h3 style={{ textAlign: 'left' }}>user name | id</h3> */}
-        <h3 style={{ textAlign: 'left' }}>user name</h3>
-        <ol>
+        <h3 style={{ textAlign: 'left' }}>Search students by user name: <StudentSearch /></h3>
+        <table className='student-data'>
+          <tbody>
+          <tr>
+            <th rowSpan='2'>
+              {this.props.students.length !== 0 && <h3>user name</h3>}
+            </th>
+            <th colSpan='2'>
+                {this.props.students.length !== 0 && <h3>student data</h3>}
+            </th>
+          </tr>
+            {this.props.students.length !== 0 && <tr><th>name</th><th>surname</th></tr>}
           {
             students.map(
               student => (
-                <li key={student.firstChild.innerHTML} style={{ textAlign: 'left' }}>
-                  {/* <span>{student.getElementsByTagName('username')[0].innerHTML}</span> | <span>{student.firstChild.innerHTML}</span> */}
-                  <span>{student.getElementsByTagName('username')[0].innerHTML}</span>
-                </li>
+                <tr key={student.firstChild.innerHTML} onClick={this.handleClick} className='student'>
+                  <td>{student.getElementsByTagName('username')[0].innerHTML}</td>
+                  <td>
+                    {student.getElementsByTagName('personal_data')[0].innerHTML && JSON.parse(student.getElementsByTagName('personal_data')[0].innerHTML).adres.name}
+                  </td>
+                  <td>
+                    {student.getElementsByTagName('personal_data')[0].innerHTML && JSON.parse(student.getElementsByTagName('personal_data')[0].innerHTML).adres.surname}
+                  </td>
+                </tr>
               )
             )
           }
-        </ol>
+          </tbody>
+        </table>
       </div>
     )
   }
