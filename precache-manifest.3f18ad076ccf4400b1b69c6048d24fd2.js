4ce74957760c797b2a48c8c5805d453a55c7cf37
Jarek Figura <jarek.figura@icloud.com>
2019-09-17 22:34:01 +0200

    Updates

--- precache-manifest.3f18ad076ccf4400b1b69c6048d24fd2.js
+++ precache-manifest.3f18ad076ccf4400b1b69c6048d24fd2.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a9d45107b76dcd0c79d26a53a54cd070",
+    "revision": "02bfbc93c86305500f8af45e4ce9b60f",
     "url": "/school-export-xml/index.html"
   },
   {
-    "revision": "68d81eafe6f7667052ed",
-    "url": "/school-export-xml/static/css/main.2c3aab23.chunk.css"
+    "revision": "97ccb83950042bec4304",
+    "url": "/school-export-xml/static/css/main.0ff3bfc5.chunk.css"
   },
   {
     "revision": "d164e8e6c2b71988ad96",
     "url": "/school-export-xml/static/js/2.2d4fb418.chunk.js"
   },
   {
-    "revision": "68d81eafe6f7667052ed",
-    "url": "/school-export-xml/static/js/main.7da910cd.chunk.js"
+    "revision": "97ccb83950042bec4304",
+    "url": "/school-export-xml/static/js/main.3f0bf6d8.chunk.js"
   },
   {
     "revision": "73ffb25aa034136619f5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/school-export-xml/precache-manifest.2f685b22b3d4b396202c57f2fa8782e5.js"
+  "/school-export-xml/precache-manifest.3f18ad076ccf4400b1b69c6048d24fd2.js"
 );
 
 self.addEventListener('message', (event) => {
