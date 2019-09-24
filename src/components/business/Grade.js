5d51f06f5134744ce37c3855aca15a760d76c53f
jarek-figura <jarek.figura@icloud.com>
2019-09-24 13:44:03 +0200

    removed "students Average grade" (commented)

--- src/components/business/Grade.js
+++ src/components/business/Grade.js
@@ -5,9 +5,12 @@ import { withXML } from '../contexts/XML';
 // TODO: OK - Dodać imię i nazwisko ucznia obok 'user name' przy ocenach
 // TODO: OK - Kliknięcie ucznia powinno pokazać tylko te klasy, w któych miał oceny (w obu semestrach); wszystko inne ukryte, bo teraz jest nieintuicyjnie
 // TODO: OK - Dodać ocenę końcową/semestralną
-// TODO: line 82 - dlaczego działa z console.log, a bez tego nie działą dobrze?
+// TODO: line 82 - dlaczego działa z console.log, a bez tego nie działa dobrze?
 // TODO: Dodać frekwencję - tylko info o statusie innym niż "obecny" + data + godzina lekcyjna
 // TODO: Dodać tekst 'Parsing XML ...' na zmianę stanu komponentów
+// TODO: Dodać filtrowanie przedmiotów?
+
+// INFO: ocena średnia (Average) zakomentowana tymczasowo; dodatkowo powinny być w niej uwzględniane wagi
 
 export class Grade extends Component {
 
@@ -42,7 +45,7 @@ export class Grade extends Component {
     }
 
     let stdGrdObj = {};
-    let stdSummaryGrade = [];
+    // let stdSummaryGrade = [];
     let stdFinalGrade = '';
 
     return (
@@ -59,7 +62,7 @@ export class Grade extends Component {
                 )
               )
             }
-            <th style={{ borderLeft: '1px solid #bbb' }}>Average</th>
+            {/* <th style={{ borderLeft: '1px solid #bbb' }}>Average</th> */}
             <th style={{ borderLeft: '1px solid #bbb', color: 'maroon' }}>Final</th>
           </tr>
           {
@@ -68,34 +71,34 @@ export class Grade extends Component {
                 (clickedId === null) || (student.grades.length && student.student_id.toString() === clickedId)
                 ?
                   <tr key={student.student_id}>
+                    {stdFinalGrade = ''}
+                    {/* {stdSummaryGrade = []} */}
                     <td className={`${tmpClassName[clickedId]}`}>{this.props.studentName[student.student_id]} {this.props.studentSurname[student.student_id]}</td>
                     <td style={{borderRight: '1px solid #bbb'}} className={`${tmpClassName[clickedId]}`}>{this.props.studentUserName[student.student_id]}</td>
                     {
-                      stdSummaryGrade = [],
-                      stdFinalGrade = '',
                       Object.keys(gradesLabel).map(
                         gradeId => (
                           stdGrdObj = student.grades.filter(
                             stdGrade => (stdGrade.column_id === Number(gradeId))
                           )[0],
                           gradesType && gradesType[gradeId] === 2
-                            ? stdGrdObj && (stdFinalGrade = `${stdGrdObj.label ? stdGrdObj.label : '0'} | ${stdGrdObj.percentage}%`, console.log(stdFinalGrade))
-                            : <td key={gradeId} style={{ whiteSpace: 'nowrap' }}>
-                              {
-                                stdGrdObj && gradesType && gradesType[gradeId] !== 2
-                                ? (
-                                    stdGrdObj.included_in_the_average && stdSummaryGrade.push(stdGrdObj.percentage),
-                                    `${stdGrdObj.label ? stdGrdObj.label : '0'} | ${stdGrdObj.percentage}%`
-                                  )
-                                : '—'
-                              }
-                              </td>
+                          ? (stdGrdObj && (stdFinalGrade = `${stdGrdObj.label ? stdGrdObj.label : '0'} | ${stdGrdObj.percentage}%`), console.log())
+                          : <td key={gradeId} style={{ whiteSpace: 'nowrap' }}>
+                            {
+                              stdGrdObj && gradesType && gradesType[gradeId] !== 2
+                              ? (
+                                  // stdGrdObj.included_in_the_average && stdSummaryGrade.push(stdGrdObj.percentage),
+                                  `${stdGrdObj.label ? stdGrdObj.label : '0'} | ${stdGrdObj.percentage}%`
+                                )
+                              : '—'
+                            }
+                            </td>
                         )
                       )
                     }
-                    <td style={{ borderLeft: '1px solid #bbb', fontWeight: 'bold', color: '#444' }}>
+                    {/* <td style={{ borderLeft: '1px solid #bbb', fontWeight: 'bold', color: '#444' }}>
                       {stdSummaryGrade.length ? (stdSummaryGrade.reduce((a, b) => a + b, 0) / stdSummaryGrade.length).toLocaleString('en-EN', { maximumFractionDigits: 1 }) : 0}%
-                    </td>
+                    </td> */}
                     <td style={{ borderLeft: '1px solid #bbb', fontWeight: 'bold', color: 'maroon', whiteSpace: 'nowrap' }}>
                       {stdFinalGrade ? stdFinalGrade : '—'}
                     </td>
