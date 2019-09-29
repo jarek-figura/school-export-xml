8ac7b3659077a5bccdaaa662fc6626c76c512c6a
Jarek Figura <jarek.figura@icloud.com>
2019-09-29 11:57:48 +0200

    added more teachers (if exist) to one subject

--- src/components/business/Clasa.js
+++ src/components/business/Clasa.js
@@ -3,7 +3,7 @@ import { withXML } from '../contexts/XML';
 import Grade from './Grade';
 import filterSubjects from './SubjectFilter';
 
-// TODO: dodać więcej, niż jednego nauczyciela do przedmiotu
+// TODO: OK - dodać więcej, niż jednego nauczyciela do przedmiotu
 // TODO: dodać filtrowanie nauczycieli danych przedmiotów, wychowawców nie filtrować
 
 export class Clasa extends Component {
@@ -14,28 +14,36 @@ export class Clasa extends Component {
     const subjects = filterSubjects(clasa, this.props.searchSubject);
 
     let subjectId = 0;
+    let teachersTable = [];
     return (
       <div>
-        {
-          subjects.map(
-            subject => (
-              <span key={subjectId++}>
-                {subject.querySelector('grades').innerHTML.length
-                  ? <Fragment>
-                      <h4 style={{ color: '#666' }}>
-                        {subject.querySelector('name').innerHTML} - {subject.querySelector('teachers').querySelector('id') &&
-                          this.props.teacherName[subject.querySelector('teachers').querySelector('id').innerHTML]}&nbsp;
-                        {subject.querySelector('teachers').querySelector('id') && 
-                          this.props.teacherSurname[subject.querySelector('teachers').querySelector('id').innerHTML]}
-                      </h4>
-                      <Grade subject={subject} />
-                    </Fragment>
-                  : <h4 style={{ color: 'silver' }}>no grades - {subject.querySelector('name').innerHTML}</h4>
-                }
-              </span>
-            )
+      {
+        subjects.map(
+          subject => (
+            <span key={subjectId++}>
+            {subject.querySelector('grades').innerHTML.length
+              ? <Fragment>
+                  <h4 style={{ color: '#666' }}>
+                  {subject.querySelector('name').innerHTML} - {
+                    teachersTable = Array.from(subject.querySelector('teachers').querySelectorAll('teacher')),
+                    teachersTable && teachersTable.map(
+                      (teacher, idx) => (
+                        <span key={teacher.querySelector('id').innerHTML}>
+                          {this.props.teacherName[teacher.querySelector('id').innerHTML]}&nbsp;{this.props.teacherSurname[teacher.querySelector('id').innerHTML]}
+                          {teachersTable.length > 1 && idx < teachersTable.length - 1 ? ', ' : ''}
+                        </span>
+                      )
+                    )
+                  }
+                  </h4>
+                  <Grade subject={subject} />
+                </Fragment>
+              : <h4 style={{ color: 'silver' }}>no grades - {subject.querySelector('name').innerHTML}</h4>
+            }
+            </span>
           )
-        }
+        )
+      }
       </div>
     );
   }
