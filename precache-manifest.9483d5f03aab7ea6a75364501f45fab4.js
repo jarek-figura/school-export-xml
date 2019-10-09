9917fe349aac5be5359ee44ee5b4de3cf7a91063
Jarek Figura <jarek.figura@icloud.com>
2019-10-10 00:22:11 +0200

    Updates

---
+++ precache-manifest.9483d5f03aab7ea6a75364501f45fab4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b567b23b32088c9928e763d04b21718",
+    "url": "/school-export-xml/index.html"
+  },
+  {
+    "revision": "434c6148b516b7a76df5",
+    "url": "/school-export-xml/static/css/main.c762e099.chunk.css"
+  },
+  {
+    "revision": "e0550766daab924fd91d",
+    "url": "/school-export-xml/static/js/2.23238b53.chunk.js"
+  },
+  {
+    "revision": "434c6148b516b7a76df5",
+    "url": "/school-export-xml/static/js/main.10326bb8.chunk.js"
+  },
+  {
+    "revision": "55602e0c95f5290d84f8",
+    "url": "/school-export-xml/static/js/runtime~main.74216a7d.js"
+  }
+]);
