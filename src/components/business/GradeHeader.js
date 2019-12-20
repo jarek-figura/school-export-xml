8422cccb9d3cf98cfdc8c3a095c0fb65fe6571ca
jarek-figura <jarek.figura@icloud.com>
2019-12-20 14:30:35 +0100

    GradeHeader.js as functional component

--- src/components/business/GradeHeader.js
+++ src/components/business/GradeHeader.js
@@ -1,37 +1,33 @@
-import React, { PureComponent, Fragment } from 'react';
-import { withXML } from '../contexts/XML';
+import React, {Fragment} from 'react';
 
 import Box from '@material-ui/core/Box';
 import TableCell from '@material-ui/core/TableCell';
 import Tooltip from '@material-ui/core/Tooltip';
 
-export class GradeHeader extends PureComponent {
+function GradeHeader(props) {
 
-  render() {
+  const grades = props.gradesFromGrade;
 
-    const grades = this.props.gradesFromGrade;
-
-    return (
-      <Fragment>
-        {
-          grades && grades.map(
-            grade => (
-              grade.type !== 2 &&
-              <TableCell key={grade.id}>
-                <Tooltip
-                  title={grade.label.length ? grade.description : '———'}
-                  placement='top'
-                >
-                  <Box fontSize={16}>{grade.label.length ? grade.label : '———'}</Box>
-                </Tooltip>
-              </TableCell>
-            )
+  return (
+    <Fragment>
+      {
+        grades && grades.map(
+          grade => (
+            grade.type !== 2 &&
+            <TableCell key={grade.id}>
+              <Tooltip
+                title={grade.label.length ? grade.description : '———'}
+                placement='top'
+              >
+                <Box fontSize={16}>{grade.label.length ? grade.label : '———'}</Box>
+              </Tooltip>
+            </TableCell>
           )
-        }
-        <TableCell style={{ borderLeft: '1px solid #e0e0e0', borderRight: '1px solid #e0e0e0', color: 'maroon' }}><Box fontSize={16}>Ocena<br />semestralna</Box></TableCell>
-      </Fragment>
-    )
-  }
+        )
+      }
+      <TableCell style={{ borderLeft: '1px solid #e0e0e0', borderRight: '1px solid #e0e0e0', color: 'maroon' }}><Box fontSize={16}>Ocena<br />semestralna</Box></TableCell>
+    </Fragment>
+  )
 }
 
-export default withXML(GradeHeader);
+export default GradeHeader;
