c915f2a99cd5b74679be6f98f370324463ed2583
Jarek Figura <jarek.figura@icloud.com>
2019-12-28 17:14:09 +0100

    wip: optimizations - moving from Array to List

--- src/components/business/GradeData.js
+++ src/components/business/GradeData.js
@@ -6,8 +6,7 @@ import TableCell from '@material-ui/core/TableCell';
 function GradeData(props) {
 
   const grades = props.gradesFromGrade;
-  const gradesId = L.pluck('id', grades);
-  console.log(gradesId); return null;
+  const gradesId = L.toArray(L.pluck('id', grades));
 
   const student = props.student;
   const stdGrades = L.from(student.grades);
@@ -22,23 +21,22 @@ function GradeData(props) {
 
   return (
     <Fragment>
-      {/* { stdFinalGrade = null } */}
+      { stdFinalGrade = null }
       {
-        L.map(
+        gradesId.map(
           gradeId => (    // grade header id
-            console.log(gradeId)
             // eslint-disable-next-line
-            // [stdObj] = L.filter(stdGrade => stdGrade.column_id === gradeId, stdGrades),
-            // gradesType[gradeId] !== 2
-            //   ? <TableCell key={gradeId}>
-            //     {
-            //       stdObj && gradesType[gradeId] !== 2
-            //         ? `${stdObj.label ? stdObj.label : '0'} | ${stdObj.percentage}%`
-            //         : '—'
-            //     }
-            //     </TableCell>
-            //   : (stdFinalGrade = stdObj && `${stdObj.label ? stdObj.label : '0'} | ${stdObj.percentage}%`, null)
-          ), gradesId
+            [stdObj] = L.filter(stdGrade => stdGrade.column_id === gradeId, stdGrades),
+            gradesType[gradeId] !== 2
+              ? <TableCell key={gradeId}>
+                {
+                  stdObj && gradesType[gradeId] !== 2
+                    ? `${stdObj.label ? stdObj.label : '0'} | ${stdObj.percentage}%`
+                    : '—'
+                }
+                </TableCell>
+              : (stdFinalGrade = stdObj && `${stdObj.label ? stdObj.label : '0'} | ${stdObj.percentage}%`, null)
+          )
         )
       }
       <TableCell style={{ borderLeft: '1px solid #e0e0e0', borderRight: '1px solid #e0e0e0', fontWeight: 'bold', color: 'maroon', whiteSpace: 'nowrap' }}>
